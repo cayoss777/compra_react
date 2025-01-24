696f9819f2dc58604a2504c829d9b5bb6c2e28af
cayoss777 <cayoss.77@gmail.com>
2025-01-24 05:08:28 -0500

    NavBar

---
+++ src/Carrito.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import { NavBar } from "./componentes/NavBar"
+export const Carrito = () => {
+  return (
+    <>
+   
+        <div>
+            <h5>Carrito compra web con React</h5>
+            <NavBar>
+
+            </NavBar>
+        <Routes>
+            <Route>
+
+            </Route>
+        </Routes>
+        </div>
+    </>
+  )
+}
